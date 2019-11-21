b4b904d3126a50bbd07081dc379985fabeb82c84
sato-kai <h175bkznpicloud.com>
2019-11-22 05:05:28 +0900

    いいね機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create(text: comment_params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
     redirect_to tweet_path(comment.tweet.id)
+    flash[:notice] = "tweetにコメントしました"
   end
 
   private
