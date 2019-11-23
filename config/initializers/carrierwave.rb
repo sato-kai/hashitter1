1773bf08011647ebaf73019aeec386f9d8614c89
sato-kai <h175bkznpicloud.com>
2019-11-23 19:28:40 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,8 +14,8 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1' # 東京はap-northeast-1
     }
-    config.fog_directory  = 'your-bucket-name' #S3のバケット名
-    config.asset_host = 'https://s3-your_region.amazonaws.com/your_bucket_name'
+    config.fog_directory  = 'hashitter' #S3のバケット名
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/hashitter'
   else
     config.storage :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
