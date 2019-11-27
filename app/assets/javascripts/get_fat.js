34cbe051ac5f926539fa9ecb63386df124e661f0
sato-kai <h175bkznpicloud.com>
2019-11-27 19:03:18 +0900

    sign-upアニメーションの編集

--- app/assets/javascripts/get_fat.js
+++ app/assets/javascripts/get_fat.js
@@ -4,13 +4,13 @@ $(document).on("turbolinks:load",function(){
     $('.large-avatar').hide();
     $('.large-avatar').each(function(i){
       $(this).delay(4000 * i).fadeIn(3000);
-      $(this).delay(3000 * i).fadeOut(3000);
+      $(this).delay(2000 * i).fadeOut(2000);
     })
     $('.avatar-comment').hide();
     $('.avatar-comment').each(function(i){
       $(this).delay(5000 * i).fadeIn(5000);
       $(this).delay(100 * i).fadeOut(100);
     })
-    $('.sign-up-after-announce').hide().delay(20000).fadeIn(1000)
+    $('.sign-up-after-announce').hide().delay(17000).fadeIn(1000)
   }
 });
