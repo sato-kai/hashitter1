355141f1eb531aff8de0c53de1f00e7e6183c996
sato-kai <h175bkznpicloud.com>
2019-12-06 16:48:58 +0900

    バリデーションを新規作成のみへ

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   }
 
   VALID_EMAIL_REGEX =   /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, presence: true
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
-  validates :password, presence: true
+  validates :name, presence: true, on: :create
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, on: :create
+  validates :password, presence: true, on: :create
 end
